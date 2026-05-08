245e6d93fcb548c2a7f1278220fb60a864bc6c25
Pengsoo15 <159673902+Pengsoo15@users.noreply.github.com>
2026-05-08 10:29:20 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.__ENV = {
-    API_KEY: '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',
-    API_URL: 'https://api.groq.com/openai/v1/chat/completions',
-    MODEL: 'llama-3.3-70b-versatile'
-};
